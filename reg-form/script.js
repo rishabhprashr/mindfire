7aa0cee307a7e9dbf481e922b3e778d546fe02ea
Rishabh Sharma <rishabhprashr@gmail.com>
2021-02-14 18:30:08 +0530

    listeners added

--- reg-form/script.js
+++ reg-form/script.js
@@ -31,55 +31,55 @@ function func(){
   
 }
 
-function searchUser() {
-  console.log(ulist);
+// function searchUser() {
+//   console.log(ulist);
 
 
-  var input, filter, li, ul, i, txtValue;
-  input = document.getElementById("selectname");
-  console.log(input.value + " = val" + (input.value === null) + (input.value === ""));
-  ul = document.getElementById("myUL");
-  ul.innerHTML = "";
-  filter = input.value.toUpperCase();
+//   var input, filter, li, ul, i, txtValue;
+//   input = document.getElementById("selectname");
+//   // console.log(input.value + " = val" + (input.value === null) + (input.value === ""));
+//   ul = document.getElementById("myUL");
+//   ul.innerHTML = "";
+//   filter = input.value.toUpperCase();
 
-  for (i = 0; i < ulist.length; i++) {
-    txtValue = ulist[i];
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      var li = document.createElement("li");
-      li.appendChild(document.createTextNode(ulist[i]));
-      li.setAttribute('id',ulist[i]);
-      li.addEventListener("click", function() {
-        console.log(ulist[i]);
-        document.getElementById("selectname").style.display=ulist[i];
+//   for (i = 0; i < ulist.length; i++) {
+//     txtValue = ulist[i];
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       var li = document.createElement("li");
+//       li.appendChild(document.createTextNode(ulist[i]));
+//       li.setAttribute('id',ulist[i]);
+//       li.addEventListener("click", function() {
+//         console.log(ulist[i]);
+//         document.getElementById("selectname").style.display=ulist[i];
         
-      });
+//       });
       
-      ul.appendChild(li);
-    }
-    //  else {
-    // //   // var li = document.getElementById(ulist[i]);
-    // //   // console.log(li);
-    // //   // ul.removeChild(li);
-    // //   // document.getElementById(ulist[i]).innerHTML = "";
-    //  }
-  }
-  if (input.value === ""){
-    console.log("yo");
-    func();
+//       ul.appendChild(li);
+//     }
+//     //  else {
+//     // //   // var li = document.getElementById(ulist[i]);
+//     // //   // console.log(li);
+//     // //   // ul.removeChild(li);
+//     // //   // document.getElementById(ulist[i]).innerHTML = "";
+//     //  }
+//   }
+//   if (input.value === ""){
+//     console.log("yo");
+//     func();
     
-  }
-}
+//   }
+// }
 
 
 
 changeFunc(0, "country");
 
-function selectli(l){
-  console.log("onclick");
-  var input = document.getElementById("selectname");
-  input.style.display=l;
-  document.getElementById("selectname").value = l;
-}
+// function selectli(l){
+//   console.log("onclick");
+//   var input = document.getElementById("selectname");
+//   input.style.display=l;
+//   document.getElementById("selectname").value = l;
+// }
 
 
 
@@ -125,4 +125,90 @@ function changeFunc($i, $p) {
   }
 }
 
-
+function autocomplete(inp, arr) {
+  var currentFocus;
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      this.parentNode.appendChild(a);
+      for (i = 0; i < arr.length; i++) {
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          b = document.createElement("DIV");
+          
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+         
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          
+          b.addEventListener("click", function(e) {
+             
+              inp.value = this.getElementsByTagName("input")[0].value;
+              
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+autocomplete(document.getElementById("selectname"), ulist);
