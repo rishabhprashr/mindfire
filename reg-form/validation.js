f4588d6834d7e5b00f8079645cc98b5db8650f6d
Rishabh Sharma <rishabhprashr@gmail.com>
2021-02-16 16:19:12 +0530

    validation added

--- reg-form/validation.js
+++ reg-form/validation.js
@@ -3,12 +3,22 @@ document.getElementById("selectname").addEventListener("keyup", check);
 
 function check($p) {
   var val = document.getElementById($p).value;
-  if(!val || !val.length) {
+  if(!val || !val.length || val=="email") {
     return;
   }
+  // var regex=/[@.:;]/gi;
+  // if(regex.test(val) && (val.length!=0 || val!="")){
+  //   document.getElementById($p).classList.remove("invalid");
+  //   document.getElementById($p).classList.add("valid");
+
+  // }else{
+  //   document.getElementById($p).classList.remove("valid");
+  //   document.getElementById($p).classList.add("invalid");
+
+  // }
   
   
-  if(val.length==0 || val=="") {
+  if(val.length==0 || val=="" ) {
     document.getElementById($p).classList.remove("valid");
     document.getElementById($p).classList.add("invalid");
   } else {
@@ -17,3 +27,17 @@ function check($p) {
   }
   
 }
+
+$(document).ready(function() {
+  $('input').characterCounter();
+});
+
+const email = document.getElementById("email");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an e-mail address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
